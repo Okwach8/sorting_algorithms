568c09be5380f782cb58dc179becc4ea18d470fe
Okwach8 <westher188@gmail.com>
2022-07-19 17:48:28 +0300

    algorithms

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,68 +1,94 @@
 #include "sort.h"
 
 /**
- * top_down_merge - accessory method to sort and merge subarrays
- * @array: array to be sorted
- * @begin: lower index of subarray
- * @end: upper index of subarray
- * @b_array: sorted values
+ * partition - function that partitions an array.
+ *
+ * @array: pointer to array of integers to be partitioned.
+ * @size: number of integers in array.
+ * @beg: start of array.
+ * @end: end of array.
+ *
+ * Return: The partion index.
  */
-void top_down_merge(int *array, size_t begin, size_t end, int *b_array)
+int partition(int *array, int beg, int end, int size)
 {
-	size_t i = begin, j, k, mid;
+	/* select the right most element as pivot */
+	int pivot = array[end];
 
-	j = mid = (begin + end) / 2;
-	printf("Merging...\n");
-	printf("[left]: ");
-	print_array(array + begin, mid - begin);
-	printf("[right]: ");
-	print_array(array + mid, end - mid);
-	for (k = begin; k < end; k++)
-		if (i < mid && (j >= end || array[i] <= array[j]))
-			b_array[k] = array[i++];
-		else
-			b_array[k] = array[j++];
-	printf("[Done]: ");
-	print_array(b_array + begin, end - begin);
+	/* set partition index as start initially */
+	int partition_index = beg;
+
+	int i, temp;
+
+	for (i = beg; i < end; i++)
+	{
+		if (array[i] <= pivot)
+		{
+			if (partition_index != i)
+			{
+				temp = array[partition_index];
+				array[partition_index] = array[i];
+				array[i] = temp;
+				print_array(array, size);
+			}
+			partition_index++;
+		}
+	}
+	if (partition_index != end)
+	{
+		/* swap pivot with element at partion index */
+		temp = array[partition_index];
+		array[partition_index] = array[end];
+		array[end] = temp;
+		print_array(array, size);
+	}
+	return (partition_index);
 }
 
 /**
- * top_down_split_merge - recursively splits array down to sortable subarrays
- * @array: int array to be sorted
- * @begin: lower index of subarray
- * @end: upper index of subarray
- * @b_array: the sorted input array
+ * quickSort - function that sorts an array of integers using recursion.
+ *
+ * @array: pointer to array of integers to be sorted.
+ * @size: number of integers in array.
+ * @beg: start of array.
+ * @end: end of array.
+ *
+ * Return: No return.
  */
-void top_down_split_merge(int *array, size_t begin, size_t end, int *b_array)
+void quickSort(int *array, int beg, int end, int size)
 {
-	size_t mid;
+	int partion_index;
 
-	mid = (begin + end) / 2;
-	if (end - begin < 2)
-		return;
-	top_down_split_merge(b_array, begin, mid, array);
-	top_down_split_merge(b_array, mid, end, array);
-	top_down_merge(array, begin, end, b_array);
+	if (beg < end)
+	{
+		partion_index = partition(array, beg, end, size);
+
+		/* recursive call on the left of pivot */
+		quickSort(array, beg, partion_index - 1, size);
+
+		/* recursive call on the right of pivot */
+		quickSort(array, partion_index + 1, end, size);
+	}
 }
 
 /**
- * merge_sort - sorts int array using merge sort
- * @array: int array to be sorted
- * @size: size of int array
+ * quick_sort - sorts an array of integers in ascending order using the,
+ * Quick sort sort algorithm.
+ *
+ * @array: pointer to array of integers to be sorted.
+ * @size: number of integers in array.
+ *
+ * Implement the Lomuto partition scheme.
+ * The pivot should always be the last element of the partition being sorted.
+ * Print the array after each time you swap two elements.
+ *
+ * Return: No return.
  */
-void merge_sort(int *array, size_t size)
+void quick_sort(int *array, size_t size)
 {
-	int *b_array;
-	size_t i;
-
+	/* An array does not need to be sorted if its size is less than 2 */
 	if (!array || size < 2)
 		return;
 
-	b_array = malloc(sizeof(int) * size);
-	if (!b_array)
-		return;
-	for (i = 0; i < size; i++)
-		b_array[i] = array[i];
-	top_down_split_merge(b_array, 0, size, array);
-	free(b_array);
+	quickSort(array, 0, size - 1, size);
 }
